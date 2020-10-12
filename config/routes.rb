7f5f66db1013e0fbf7daa6efde3ca29c31987313
”Kanta” <k.kanta0826@gmail.com>
2020-10-12 17:58:38 +0900

    10/12

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'pages/help'
+  
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
 end
